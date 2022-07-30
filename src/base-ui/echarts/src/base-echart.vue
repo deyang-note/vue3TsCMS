39bfb53cc67901435214e8cde4028bc9f5861891
passion <1771886180@qq.com>
2022-07-30 11:57:42 +0800

    feat(echart): base-echart封装

---
+++ src/base-ui/echarts/src/base-echart.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="base-echart">
+    <div ref="echartDivRef" :style="{ width, height }"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import * as echarts from "echarts"
+import { ref, onMounted, withDefaults } from "vue"
+import { EChartsOption } from "echarts"
+
+const props = withDefaults(
+  defineProps<{
+    options: EChartsOption
+    width?: string
+    height?: string
+  }>(),
+  {
+    width: "100%",
+    height: "360px"
+  }
+)
+
+const echartDivRef = ref<HTMLElement>()
+onMounted(() => {
+  const echartInstance = echarts.init(echartDivRef.value!)
+
+  echartInstance.setOption(props.options)
+})
+</script>
+
+<style scoped></style>
--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -7,7 +7,11 @@
     </el-row>
 
     <el-row :gutter="10" class="content-row">
-      <el-col :span="12"><dy-card title="分类商品数量" /></el-col>
+      <el-col :span="12">
+        <dy-card title="分类商品数量">
+          <base-echart :options="options" />
+        </dy-card>
+      </el-col>
       <el-col :span="12"><dy-card title="分类商品的收藏" /></el-col>
     </el-row>
   </div>
@@ -16,9 +20,30 @@
 <script lang="ts" setup>
 import { useStore } from "vuex"
 import DyCard from "@/base-ui/card"
+import BaseEchart from "@/base-ui/echarts"
 
 const store = useStore()
 store.dispatch("dashboard/getDashboardDataAction")
+
+const options = {
+  xAxis: {
+    type: "category",
+    data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+  },
+  yAxis: {
+    type: "value"
+  },
+  series: [
+    {
+      data: [120, 200, 150, 80, 70, 110, 130],
+      type: "bar",
+      showBackground: true,
+      backgroundStyle: {
+        color: "rgba(180, 180, 180, 0.2)"
+      }
+    }
+  ]
+}
 </script>
 
 <style scoped lang="less">
